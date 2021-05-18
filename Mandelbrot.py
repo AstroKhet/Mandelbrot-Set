db2941b0f711a0656b2cdd6d8951fd2bd1ff867e
SaxyCat <67085313+SaxyCat@users.noreply.github.com>
2021-05-18 18:17:06 +0800

    Update Mandelbrot.py
    renamed some variables

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -5,10 +5,10 @@ import time
 width = 10000
 height = 6666
 
-RE_START = -2
-RE_END = 1
-IM_START = -1
-IM_END = 1
+real_start = -2
+real_end = 1
+imag_start = -1
+imag_end = 1
 iterations = 80
 
 img = Image.new('RGB', (width, height), (0, 0, 0))
@@ -16,9 +16,9 @@ canvas = ImageDraw.Draw(img)
 
 
 def heat(n):
-    # Smaller n is, closer to white/yellow  (R^, G^-, B^--)
-    # Larger n is, closer to black, blue    (Rv, Gv-, Bv--)
-    # Honestly I have no idea what to do
+    # Smaller n is, closer to white/yellow  
+    # Larger n is, closer to black, blue    
+    # Honestly I have no idea what I'm doing
     r = min(round((255 * n)/(0.6*iterations)), 255)
     g = min(round((255 * n)/(0.3*iterations)), 255)
     b = min(round((255 * n)/(0.15*iterations)), 255)
@@ -40,12 +40,12 @@ def get_colour(C: complex):
 start = time.time()
 for y in range(height):
     for x in range(width):
-        c = complex(RE_START + (x / width) * (RE_END - RE_START),
-                    IM_START + (y / height) * (IM_END - IM_START))
+        c = complex(real_start + (x / width) * (real_end - real_start),
+                    imag_start + (y / height) * (imag_end - imag_start))
 
         col = get_colour(c)
         canvas.point([x, y], col)
 end = time.time()
 
-print(end - start)
+print(f"Time taken: {round(end - start, 2)} seconds")
 img.convert('RGB').save('Gallery/image.png', 'PNG')
